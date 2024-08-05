342f3d1b05549896f48436ae348542060d33b7d6
Sergey Kazakov <kazaimazai@gmail.com>
2024-08-05 14:10:37 +0100

    API service prototype

---
+++ Packages/MainFeed/Sources/MainFeed/Services/PexelAPIService.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sergey Kazakov on 04/08/2024.
+//
+
+import Foundation
+
+@MainActor
+@Observable
+final class PexelAPIService {
+    
+    func fetch(page: Int, limit: Int) async throws -> [Picture] {
+        
+    }
+}
---
+++ Packages/MainFeed/Sources/MainFeed/Services/Types.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sergey Kazakov on 05/08/2024.
+//
+
+import Foundation
+import DomainModel
+
+typealias Picture = DomainModel.Picture
