d17d31030e31237785199d8e11834dec96fecc4a
Sergey Kazakov <kazaimazai@gmail.com>
2024-08-07 17:47:32 +0100

    card style refactoring

--- Packages/DesignSystem/Sources/DesignSystem/UIComponents/StylingExtensions/CardView.swift
+++ Packages/DesignSystem/Sources/DesignSystem/UIComponents/StylingExtensions/CardView.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 
 public extension View {
-    func card() -> some View {
+    func cardStyle() -> some View {
         self.cornered(.theme.corners.large)
             .shadow(.theme.largeDropShadow)
     }
     
-    func card(with cornerRatio: CGFloat) -> some View {
+    func cardStyle(with cornerRatio: CGFloat) -> some View {
         self.cornered((1.0 - cornerRatio) * .theme.corners.large)
             .shadow(.theme.largeDropShadow)
     }
